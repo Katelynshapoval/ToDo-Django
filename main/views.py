c8a6d75aa2f35b1cf3aac67ae6adbec69cae9a88
Katelyn Shapoval <katelyn.shapoval@gmail.com>
2024-10-03 10:41:40 +0200

    models

--- main/views.py
+++ main/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import ToDoList, Item
 
 # Create your views here.
-from django.http import HttpResponse
 
 
-def index(request):
-    return HttpResponse("Katya Commit")
+def index(request, id):
+    ls = ToDoList.objects.get(id=id)
+    item = ls.item_set.get(id=1)
+    return HttpResponse("%s, %s" % (ls.name, str(item.text)))
